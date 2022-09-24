07664e63edc6b809da21de60d6d42014c31480c3
Artem <Palorel@yandex.ru>
2022-09-24 15:50:41 +0300

    Сделаны тесты + финальная проверка

--- smartQSort/main.c
+++ smartQSort/main.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <stdbool.h>
 
 int scanOne();
+void qSort(int* arrayOut, int startArray, int endArray);
+int partition(int* arrayOut, int startArray, int endArray);
+void insertSort(int* arrayOut, int startArray, int endArray);
+
+bool testCorrectValue() {
+	int arrayOut[15] = { 28, 96, 34, 70, 62, 59, 100, 9, 59, 91, 90, 64, 16, 48, 70 };
+	qSort(arrayOut, 0, 14);
+	for (int i = 1; i < 15; ++i) {
+		if (arrayOut[i] < arrayOut[i - 1]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+bool testAnotherValue() {
+	int arrayOut[3] = { 1, 1, 1 };
+	qSort(arrayOut, 0, 2);
+	for (int i = 1; i < 3; ++i) {
+		if (arrayOut[i] < arrayOut[i - 1]) {
+			return false;
+		}
+	}
+	return true;
+}
 
 void insertSort(int* arrayOut, int startArray, int endArray) {
 	if (arrayOut != NULL) {
@@ -53,7 +79,11 @@ void qSort(int* arrayOut, int startArray, int endArray) {
 
 int main() {
 	setlocale(LC_ALL, "RUS");
-
+	if (!testAnotherValue() || !testCorrectValue()) {
+		printf("%s", "Тесты не пройдены... \n");
+	} else {
+		printf("%s", "Тесты пройдены \n");
+	}
 	printf("%s", "Введите размер массива \n");
 	int size = scanOne();
 	int* arrayOut = (int*)calloc(size, sizeof(int));
