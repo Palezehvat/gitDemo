3c0cc69bcd08e0855f256b48a1dc7c8624f18394
Artem <Palorel@yandex.ru>
2022-09-28 19:10:55 +0300

    добавлена функция ввода

---
+++ BinaryNumbers/BinaryNumbers/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int scanOne();
+
+int main() {
+	int number = scanOne();
+}
+
+int scanOne() {
+	int number = 0;
+	int checkScanf = scanf("%d", &number);
+
+	while (checkScanf != 1) {
+		while (getchar() != '\n') {
+		}
+
+		printf("%s", "Ошибка... Проверьте правильность ввода \n");
+		checkScanf = scanf("%d", &number);
+	}
+
+	return number;
+}
