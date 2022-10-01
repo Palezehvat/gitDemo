cb3dd1fea19f188fadee1503dd3d700abe5eae11
Artem <Palorel@yandex.ru>
2022-10-02 00:20:48 +0300

    Переделал всё под тесты, чтобы не выводило на экран промежуточные результаты во время тестов

--- BinaryNumbers/BinaryNumbers/main.c
+++ BinaryNumbers/BinaryNumbers/main.c
@@ -6,7 +6,7 @@
 int scanOne();
 void print(int number);
 
-void binaryToDecimal(bool *binaryNumber)
+int binaryToDecimal(bool *binaryNumber)
 {
 	int multiplier = 1;
 	int number = 0;
@@ -15,10 +15,7 @@ void binaryToDecimal(bool *binaryNumber)
 		multiplier = multiplier << 1;
 	}
 	
-	if (binaryNumber[0]) {
-		number = number - 4096;
-	}
-	printf("%d", number);
+	return binaryNumber[0] ? number - 4096 : number;
 }
 int sizeBinary(int number) {
 	int size = 0;
@@ -43,9 +40,7 @@ void printArrayBinary(bool *binaryNumber, int size, int maxPow2) {
 	printf("\n");
 }
 
-void sumToBinary(bool *binaryNumber1, bool *binaryNumber2, int size1, int size2) {
-	bool binaryNumber3[12] = {false};
-
+void sumToBinary(bool *binaryNumber1, bool *binaryNumber2, bool binaryNumber3[], int size1, int size2) {
 	bool pastSum = false;
 	int i = 11;
 	int size3 = 0;
@@ -71,43 +66,44 @@ void sumToBinary(bool *binaryNumber1, bool *binaryNumber2, int size1, int size2)
 		binaryNumber3[0] = true;
 		size3 = 12;
 	}
-
-	printArrayBinary(binaryNumber3, size3, 12);
-	binaryToDecimal(binaryNumber3);
 }
 
-void toBinary(int number1, int number2) {
-	number1 = number1 < 0 ? 4096 + number1 : number1;
-	number2 = number2 < 0 ? 4096 + number2 : number2;
-	int size1 = sizeBinary(number1);
-	int size2 = sizeBinary(number2);
-	bool binaryNumber1[12] = {false};
-	bool binaryNumber2[12] = {false};
-
-	translateDecimalToBinary(binaryNumber1, size1, number1);
-	translateDecimalToBinary(binaryNumber2, size2, number2);
-
-	printArrayBinary(binaryNumber1, size1, 12);
-	printArrayBinary(binaryNumber2, size2, 12);
-
-	sumToBinary(binaryNumber1, binaryNumber2, size1, size2);
+int toChangeNegativeNumbers(int number) {
+	return number < 0 ? 4096 + number : number;
 }
 
 int main() {
 	setlocale(LC_ALL, "RUS");
-	printf("%s", "¬ведите два числа, которые затем будут преобразованы в двоичное представление.„исла должны быть меньше 4096 по модулю\n");
+	printf("%s", "¬ведите два числа, которые затем будут преобразованы в двоичное представление.„исла и их сумма должны быть меньше 2048 по модулю\n");
 	int number1 = scanOne();
-	while (abs(number1) >= 4096) {
+	while (abs(number1) >= 2048) {
 		printf("%s\n", "¬веденное число превышает ограничение! ¬ведите заново");
 		number1 = scanOne();
 	}
 	int number2 = scanOne();
-	while (abs(number2) >= 4096) {
+	while (abs(number2) >= 2048 || abs(number1 + number2) >= 2048) {
 		printf("%s\n", "¬веденное число превышает ограничение! ¬ведите заново");
+		number1 = scanOne();
 		number2 = scanOne();
 	}
-
-	toBinary(number1, number2);
+	number1 = toChangeNegativeNumbers(number1);
+	number2 = toChangeNegativeNumbers(number2);
+	int size1 = sizeBinary(number1);
+	int size2 = sizeBinary(number2);
+	bool *binaryNumber1 = (bool*)calloc(12, sizeof(int));
+	bool *binaryNumber2 = (bool*)calloc(12, sizeof(int));
+	translateDecimalToBinary(binaryNumber1, size1, number1);
+	translateDecimalToBinary(binaryNumber2, size2, number2);
+	printArrayBinary(binaryNumber1, size1, 12);
+	printArrayBinary(binaryNumber2, size2, 12);
+	int size3 = 12;
+	bool *binaryNumber3 = (bool*)calloc(12, sizeof(int));
+	sumToBinary(binaryNumber1, binaryNumber2,binaryNumber3, size1, size2);
+	printArrayBinary(binaryNumber3, size3, 12);
+	printf("%d", binaryToDecimal(binaryNumber3));
+	free(binaryNumber1);
+	free(binaryNumber2);
+	free(binaryNumber3);
 }
 
 void print(int number) {
