a1e75bf70b5e29a7e9152b83e1203b9640edc8d3
Artem <Palorel@yandex.ru>
2022-10-01 17:30:21 +0300

    Сделал перевод чисел в 2систему, пока работает только для натуральных чисел

--- BinaryNumbers/BinaryNumbers/main.c
+++ BinaryNumbers/BinaryNumbers/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <stdbool.h>
 
 int scanOne();
 void print(int number);
@@ -14,12 +15,31 @@ int sizeBinary(int number) {
 	return size;
 }
 
+void translateDecToBin(bool *binaryNumber, int size, int number) {
+	for (int i = size - 1; i >= 0; --i) {
+		binaryNumber[i] = (number & 1);
+		number = number >> 1;
+	}
+}
+
+void printArrayBinary(bool *binaryNumber, int size) {
+	for (int i = 0; i < size; ++i) {
+		printf("%d", binaryNumber[i]);
+	}
+	printf("\n");
+}
+
 void toBinary(int number1, int number2) {
 	int size1 = sizeBinary(number1);
 	int size2 = sizeBinary(number2);
-	int *binaryNumber1 = (int*)calloc(size1, sizeof(int));
-	int *binaryNumber2 = (int*)calloc(size2, sizeof(int));
+	bool *binaryNumber1 = (bool*)calloc(size1, sizeof(bool));
+	bool *binaryNumber2 = (bool*)calloc(size2, sizeof(bool));
+	
+	translateDecToBin(binaryNumber1, size1, number1);
+	translateDecToBin(binaryNumber2, size2, number2);
 
+	printArrayBinary(binaryNumber1, size1);
+	printArrayBinary(binaryNumber2, size2);
 
 	free(binaryNumber1);
 	free(binaryNumber2);
@@ -30,6 +50,7 @@ int main() {
 	printf("%s", "Введите два числа, которые затем будут преобразованы в двоичное представление\n");
 	int number1 = scanOne();
 	int number2 = scanOne();
+	toBinary(number1, number2);
 }
 
 void print(int number) {
