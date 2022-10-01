db898dffd9de485889f8258ac5426d948ed27103
Artem <Palorel@yandex.ru>
2022-10-01 16:44:05 +0300

    Добавил функцию подсчёта колличества разрядов чиселб, дабы в другой функции с помощью массива можно было преобразовывать большие числа

--- BinaryNumbers/BinaryNumbers/main.c
+++ BinaryNumbers/BinaryNumbers/main.c
@@ -1,10 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 int scanOne();
+void print(int number);
+
+int sizeBinary(int number) {
+	int size = 0;
+	while (number) {
+		number = number >> 1;
+		++size;
+	}
+	return size;
+}
+
+void toBinary(int number1, int number2) {
+	int size1 = sizeBinary(number1);
+	int size2 = sizeBinary(number2);
+	int *binaryNumber1 = (int*)calloc(size1, sizeof(int));
+	int *binaryNumber2 = (int*)calloc(size2, sizeof(int));
+
+
+	free(binaryNumber1);
+	free(binaryNumber2);
+}
 
 int main() {
-	int number = scanOne();
+	setlocale(LC_ALL, "RUS");
+	printf("%s", "Введите два числа, которые затем будут преобразованы в двоичное представление\n");
+	int number1 = scanOne();
+	int number2 = scanOne();
+}
+
+void print(int number) {
+	printf("%d\n", number);
 }
 
 int scanOne() {
