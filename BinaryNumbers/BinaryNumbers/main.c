9ccfeb69414d5da1f4af2be8cebf9a9b4ed07942
Artem <Palorel@yandex.ru>
2022-10-01 18:54:16 +0300

    Сделал массивы статическими и переделал расположение 0 и 1, так как легче складывать числа

--- BinaryNumbers/BinaryNumbers/main.c
+++ BinaryNumbers/BinaryNumbers/main.c
@@ -16,35 +16,49 @@ int sizeBinary(int number) {
 }
 
 void translateDecToBin(bool *binaryNumber, int size, int number) {
-	for (int i = size - 1; i >= 0; --i) {
+	for (int i = 12; i >= 13 - size; --i) {
 		binaryNumber[i] = (number & 1);
 		number = number >> 1;
 	}
 }
 
 void printArrayBinary(bool *binaryNumber, int size) {
-	for (int i = 0; i < size; ++i) {
+	for (int i = 13 - size; i < 13; ++i) {
 		printf("%d", binaryNumber[i]);
 	}
 	printf("\n");
 }
+/*
+void sumToBinary(int *binaryNumber1, int *binaryNumber2, int size1, int size2) {
+	int size3 = size1 + size2;
+	bool *binaryNumber3 = (bool*)calloc(size3, sizeof(int));
 
+	int pastSum = -1;
+	int i = max(size1, size2) - 1;
+	while (i != 0) {
+		if (i > size1) {
+			
+		} else if (i > size2) {
+
+		} else {
+
+		}
+	}
+}
+*/
 void toBinary(int number1, int number2) {
 	number1 = number1 < 0 ? 4096 - number1 : number1;
 	number2 = number2 < 0 ? 4096 - number2 : number2;
 	int size1 = sizeBinary(number1);
 	int size2 = sizeBinary(number2);
-	bool *binaryNumber1 = (bool*)calloc(size1, sizeof(bool));
-	bool *binaryNumber2 = (bool*)calloc(size2, sizeof(bool));
+	bool binaryNumber1[13] = {false};
+	bool binaryNumber2[13] = {false};
 	
 	translateDecToBin(binaryNumber1, size1, number1);
 	translateDecToBin(binaryNumber2, size2, number2);
 
 	printArrayBinary(binaryNumber1, size1);
 	printArrayBinary(binaryNumber2, size2);
-
-	free(binaryNumber1);
-	free(binaryNumber2);
 }
 
 int main() {
