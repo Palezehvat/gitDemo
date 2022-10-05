0cf18a1c0b3c201086b650704c38602301ebca4c
Artem <Palorel@yandex.ru>
2022-10-05 15:50:21 +0300

    Убрал i, так как он тут не нужен

--- fibonachiSummary/fibonachiSummary/main.c
+++ fibonachiSummary/fibonachiSummary/main.c
@@ -10,14 +10,12 @@ bool test() {
 int fibonacciIterative() {
 	int firstElement = 1;
 	int secondElement = 1;
-	int i = 0;
 	int lastSecondElement = 0;
 
 	int sumElements = 0;
 	while (secondElement < 1000000 && firstElement < 1000000) {
 		secondElement = firstElement + secondElement;
 		firstElement = secondElement - firstElement;
-		++i;
 		if (secondElement % 2 == 0) {
 			sumElements += secondElement;
 		}
