c3af4c8ad53ac8e8d3f0d7ab9abb4b20e7c0b969
Artem <Palorel@yandex.ru>
2022-10-26 18:08:52 +0300

    Сделал monkeySort

---
+++ monkeySort/monkeySort/main.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <string.h>
+#include <stdbool.h>
+#include <time.h>
+
+int scanOne();
+
+bool isSorted(int* arrayOut, int size) {
+	if (arrayOut == NULL) {
+		return false;
+	}
+
+	for (int i = 1; i < size; ++i) {
+		if (arrayOut[i] < arrayOut[i - 1]) {
+			return false;
+		}
+	}
+	return true;
+}
+
+bool monkeySort(int* arrayOut, int size) {
+	srand((unsigned int)time(NULL));
+	if (arrayOut == NULL) {
+		return false;
+	}
+	while (!isSorted(arrayOut, size)) {
+		int firstIndex = rand() % size;
+		int secondIndex = rand() % size;
+		int number = 0;
+		number = arrayOut[firstIndex];
+		arrayOut[firstIndex] = arrayOut[secondIndex];
+		arrayOut[secondIndex] = number;
+	}
+	return true;
+}
+
+int main() {
+	setlocale(LC_ALL, "RUS");
+	printf("Введите размер массива\n");
+	int size = scanOne();
+	if (size == 0) {
+		return 0;
+	}
+	int* arrayOut = (int*)calloc(size, sizeof(int));
+	if (arrayOut == NULL) {
+		printf("Ошибка выделения памяти\n");
+		return 0;
+	}
+	printf("Введите элементы массива\n");
+	for (int i = 0; i < size; ++i) {
+		arrayOut[i] = scanOne();
+	}
+	if (!monkeySort(arrayOut, size)) {
+		printf("Ошибка...\n");
+		return 0;
+	}
+	for (int i = 0; i < size; ++i) {
+		printf("%d ", arrayOut[i]);
+	}
+
+	free(arrayOut);
+}
+
+int scanOne() {
+	int number = 0;
+	int checkScanf = scanf("%d", &number);
+
+	while (checkScanf != 1) {
+		while (getchar() != '\n') {
+		}
+
+		printf("%s", "Ошибка... Проверьте правильность ввода \n");
+		checkScanf = scanf("%d", &number);
+	}
+
+	return number;
+}
