ed6c0261e942ca162d0161b24142ba4ceb02b6a2
Artem <Palorel@yandex.ru>
2022-10-26 17:42:45 +0300

    Сделал тесты и добавил закрытие файла

--- whatSymbols/whatSymbols/main.c
+++ whatSymbols/whatSymbols/main.c
@@ -4,8 +4,8 @@
 #include <string.h>
 #include <stdbool.h>
 
-void scanFile(int arrayOut[], char fileName[]) {
-	FILE* file = fopen("test.txt", "r");
+int scanFile(int arrayOut[], char fileName[]) {
+	FILE* file = fopen(fileName, "r");
 	if (file == NULL) {
 		printf("Файл не найден!");
 		return -1;
@@ -14,17 +14,34 @@ void scanFile(int arrayOut[], char fileName[]) {
 	while (fscanf(file, "%s", data) == 1) {
 		size_t sizeData = strlen(data);
 		for (int i = 0; i < sizeData; ++i) {
-			++arrayOut[(int)data];
+			++arrayOut[(int)data[i]];
 		}
 	}
+	fclose(file);
+	return 0;
 }
 
 bool test() {
+	char fileName[9] = "test.txt";
+	int arrayOut[128] = { 0 };
+	if (scanFile(arrayOut, fileName) == -1) {
+		printf("Ошибка...\n");
+		return 0;
+	}
+	if (arrayOut[97] != 3 && arrayOut[98] != 2 && arrayOut[99] != 1) {
+		return false;
+	}
 	return true;
 }
 
 int main() {
 	setlocale(LC_ALL, "RUS");
+	if (test()) {
+		printf("Тесты прошли успешно\n");
+	} else {
+		printf("Ошибка...\n");
+		return 0;
+	}
 	int arrayOut[128] = { 0 };
 	printf("Введите название файла(с расширением), он будет считан, и будет выведено колличество различных символов из строки\n");
 	char fileName[100] = { '\0' };
@@ -36,10 +53,13 @@ int main() {
 		}
 		checkScan = scanf("%s", &fileName);
 	}
-	scanFile(arrayOut, fileName);
+	if (scanFile(arrayOut, fileName) == -1) {
+		printf("Ошибка...\n");
+		return 0;
+	}
 	for (int i = 0; i < 128; ++i) {
 		if (arrayOut[i] != 0) {
-			printf("%c: %d", (char)i, arrayOut[i]);
+			printf("%c: %d\n", (char)i, arrayOut[i]);
 		}
 	}
 }
