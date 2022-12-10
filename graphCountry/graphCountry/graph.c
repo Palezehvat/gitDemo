0ad481a4448e085f04aeee5e4320182c736b2228
Artem <Palorel@yandex.ru>
2022-12-10 15:57:28 +0300

    Сделал тесты и граф

--- graphCountry/graphCountry/graph.c
+++ graphCountry/graphCountry/graph.c
@@ -5,19 +5,10 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-typedef enum Error {
-	memmoryError,
-	anotherError,
-	nullGraph,
-	outOfGraph,
-	fileError,
-	ok,
-};
-
 typedef enum TypeTown {
 	capital,
 	notBusy,
-	Busy
+	busy
 }TypeTown;
 
 typedef struct Node {
@@ -31,25 +22,26 @@ typedef struct Node {
 typedef struct Graph {
 	int numNode;
 	struct Node** nodeList;
+	int numberOfCapitals;
 }Graph;
 
 Graph* createGraph(Error errorCheck, int sizeGraph) {
 	Graph* graph = calloc(1, sizeof(Graph));
 	if (graph == NULL) {
-		errorCheck = memmoryError;
+		errorCheck = memoryError;
 		return NULL;
 	}
 	graph->numNode = sizeGraph;
 	graph->nodeList = (Node**)malloc(sizeGraph * sizeof(Node));
 	if (graph->nodeList == NULL) {
-		errorCheck = memmoryError;
+		errorCheck = memoryError;
 		free(graph);
 		return NULL;
 	}
 	for (int i = 0; i < sizeGraph; ++i) {
 		graph->nodeList[i] = calloc(1, sizeof(Node*));
 		if (graph->nodeList[i] == NULL) {
-			errorCheck = memmoryError;
+			errorCheck = memoryError;
 			for (int j = 0; j < i; ++j) {
 				free(graph->nodeList[j]);
 			}
@@ -60,6 +52,7 @@ Graph* createGraph(Error errorCheck, int sizeGraph) {
 		graph->nodeList[i][0].next = NULL;
 		graph->nodeList[i][0].onWhichControl = -1;
 		graph->nodeList[i][0].typeTown = notBusy;
+		graph->nodeList[i][0].sizeRoadWithMainVertex = 0;
 	}
 
 	return graph;
@@ -72,14 +65,14 @@ void helpToAddRoad(Graph* graph, int firstNode, int secondNode, int sizeRoad, Er
 	}
 	Node* temp = calloc(1, sizeof(Node));
 	if (temp == NULL) {
-		errorCheck = memmoryError;
+		errorCheck = memoryError;
 		for (int i = 0; i < graph->numNode; ++i) {
 			free(graph->nodeList[i]);
 		}
 		free(graph);
 		return;
 	}
-	walkerInGraph->sizeRoadWithMainVertex = sizeRoad;
+	temp->sizeRoadWithMainVertex = sizeRoad;
 	temp->vertex = secondNode;
 	temp->next = NULL;
 	temp->onWhichControl = -1;
@@ -92,6 +85,7 @@ void addRoad(Graph* graph, Error errorCheck, int firstNode, int secondNode, int 
 		errorCheck = nullGraph;
 		return;
 	}
+
 	if (firstNode >= graph->numNode || secondNode >= graph->numNode) {
 		errorCheck = outOfGraph;
 		for (int i = 0; i < graph->numNode; ++i) {
@@ -100,14 +94,22 @@ void addRoad(Graph* graph, Error errorCheck, int firstNode, int secondNode, int 
 		free(graph);
 		return;
 	}
+	
+	if (firstNode == secondNode) {
+		graph->nodeList[firstNode]->sizeRoadWithMainVertex = sizeRoad;
+		return;
+	}
+
 	helpToAddRoad(graph, firstNode, secondNode, sizeRoad, errorCheck);
 	if (errorCheck != ok) {
+		clearGraph(graph);
 		return;
 	}
 
 	if (firstNode != secondNode) {
 		helpToAddRoad(graph, secondNode, firstNode, sizeRoad, errorCheck);
 		if (errorCheck != ok) {
+			clearGraph(graph);
 			return;
 		}
 	}
@@ -119,15 +121,14 @@ void printGraph(Graph* graph) {
 	}
 	for (int i = 0; i < graph->numNode; ++i) {
 		Node* walker = graph->nodeList[i];
-		printf("%d: ", walker->vertex);
-		if (walker->next != NULL) {
+		printf("%d : ", walker->vertex);
+		if (walker->sizeRoadWithMainVertex != 0) {
 			printf("-%d- ", walker->sizeRoadWithMainVertex);
 		}
 		walker = walker->next;
 		while (walker != NULL) {
-			printf("%d ", walker->vertex); if (walker->next != NULL) {
-				printf("-%d- ", walker->sizeRoadWithMainVertex);
-			}
+			printf("%d ", walker->vertex); 
+			printf("(%d) ", walker->sizeRoadWithMainVertex);
 			walker = walker->next;
 		}
 		printf("\n");
@@ -135,25 +136,134 @@ void printGraph(Graph* graph) {
 }
 
 void addCapital(Graph* graph, int numberCapital, Error checkError) {
-	if (numberCapital >= graph->numNode) {
-		checkError = anotherError;
-		return;
-	}
 	if (graph == NULL) {
 		checkError = anotherError;
 		return;
 	}
+
+	if (numberCapital >= graph->numNode) {
+		checkError = anotherError;
+		clearGraph(graph);
+		return;
+	}
+
 	graph->nodeList[numberCapital]->typeTown = capital;
+	Node* walker = graph->nodeList[numberCapital]->next;
+	while (walker != NULL) {
+		Node* newWalker = graph->nodeList[walker->vertex];
+		while (newWalker->vertex != numberCapital) {
+			newWalker = newWalker->next;
+		}
+		newWalker->typeTown = capital;
+		walker = walker->next;
+	}
+	++graph->numberOfCapitals;
 }
 
-void recursionAddTown(Graph* graph, int* min, int* numberTown) {
-	{
+void recursionAddTown(Graph* graph, int* min, int* numberTown, int index, const int numberCapital, int localMin, Error errorCheck) {
+	Node* walker = graph->nodeList[index]->next;
+	
+	while (walker != NULL) {
+		if (walker->typeTown == busy && walker->onWhichControl == numberCapital) {
+			localMin += walker->sizeRoadWithMainVertex;
+			recursionAddTown(graph, min, numberTown, walker->vertex, numberCapital, localMin, errorCheck);
+		} else if (walker->typeTown == notBusy) {
+			localMin += walker->sizeRoadWithMainVertex;
+			if (localMin < *min || *min == -1) {
+				*min = localMin;
+				*numberTown = walker->vertex;
+			}
+			localMin -= walker->sizeRoadWithMainVertex;
+		}
+		walker = walker->next;
+	}
 
+}
+
+void addTown(Graph* graph, int numberCapital, Error errorCheck, bool* isAdded) {
+	if (graph == NULL) {
+		errorCheck = nullGraph;
+		return;
+	}
+	int min = -1;
+	int numberAddedTown = numberCapital;
+	int index = numberCapital;
+	recursionAddTown(graph, &min, &numberAddedTown, index, numberCapital, 0, errorCheck);
+	if (min != -1) {
+		graph->nodeList[numberAddedTown]->typeTown = busy;
+		graph->nodeList[numberAddedTown]->onWhichControl = numberCapital;
+		Node* walker = graph->nodeList[numberAddedTown]->next;
+		while (walker != NULL) {
+			Node* anotherWalker = graph->nodeList[walker->vertex];
+			while (anotherWalker->vertex != numberAddedTown) {
+				anotherWalker = anotherWalker->next;
+			}
+			anotherWalker->typeTown = busy;
+			anotherWalker->onWhichControl = numberCapital;
+			walker = walker->next;
+		}
+		*isAdded = true;
 	}
 }
 
-void addTown(Graph* graph, int numberTown) {
-	int min = -1;
-	int numberAddedTown = -1;
-	recursionAddTown(graph, &min, &numberTown);
+void toControllTowns(Graph* graph, Error errorCheck) {
+	if (graph == NULL) {
+		errorCheck = nullGraph;
+		return;
+	}
+	int i = 0;
+	int indexCapital = 0;
+	bool isAdded = false;
+	while (i < graph->numNode - graph->numberOfCapitals) {
+		while (graph->nodeList[indexCapital]->typeTown != capital) {
+			++indexCapital;
+			if (indexCapital == graph->numNode) {
+				indexCapital = 0;
+			}
+		}
+		isAdded = false;
+		addTown(graph, indexCapital, errorCheck, &isAdded);
+		++indexCapital;
+		if (indexCapital == graph->numNode) {
+			indexCapital = 0;
+		}
+		if (isAdded) {
+			++i;
+		}
+	}
+}
+
+void returnArrayWithControlledTowns(Graph* graph, Error errorCheck, bool** controlledTownsArray) {
+	int indexSizeTowns = 0;
+	Node* walker = graph->nodeList[0];
+	while (indexSizeTowns < graph->numNode) {
+		while (walker != NULL) {
+			if (walker->typeTown == capital) {
+				controlledTownsArray[walker->vertex][0] = true;
+			}
+			else {
+				controlledTownsArray[walker->onWhichControl][walker->vertex + 1] = true;
+			}
+			walker = walker->next;
+		}
+		walker = graph->nodeList[indexSizeTowns];
+		++indexSizeTowns;
+	}
+	return controlledTownsArray;
+}
+
+void clearGraph(Graph* graph) {
+	int i = 0;
+	while (i < graph->numNode) {
+		Node* walker = graph->nodeList[i]->next;
+		while (walker != NULL) {
+			Node* temp = walker->next;
+			free(walker);
+			walker = temp;
+		}
+//		free(graph->nodeList[i]);
+		++i;
+	}
+	free(graph->nodeList);
+	free(graph);
 }
--- graphCountry/graphCountry/main.c
+++ graphCountry/graphCountry/main.c
@@ -1,16 +1,8 @@
 #include "graph.h"
+#include "test.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-
-typedef enum Error {
-	memmoryError,
-	anotherError,
-	nullGraph,
-	outOfGraph,
-	fileError,
-	ok,
-};
 
 Error workWithGraph(const char* fileName) {
 	Error errorCheck = ok;
@@ -19,43 +11,137 @@ Error workWithGraph(const char* fileName) {
 		return fileError;
 	}
 	int numberOfTowns = 0;
-	if (fscanf(file, "%d", &numberOfTowns) != 1) {
+	if (fscanf_s(file, "%d", &numberOfTowns) != 1) {
+		fclose(file);
 		return fileError;
+	}
+	if (numberOfTowns == INT_MAX) {
+		return anotherError;
 	}
 	Graph* graph = createGraph(errorCheck, numberOfTowns);
 
 	int numberOfRoads = 0;
-	if (fscanf(file, "%d", &numberOfRoads) != 1) {
+	if (fscanf_s(file, "%d", &numberOfRoads) != 1) {
+		fclose(file);
 		return fileError;
 	}
 	int firstTown = 0;
 	int secondTown = 0;
 	int sizeRoad = 0;
 	for (int i = 0; i < numberOfRoads; ++i) {
-		if (fscanf(file, "%d %d %d", &firstTown, &secondTown, &sizeRoad) != 3) {
+		if (fscanf_s(file, "%d %d %d", &firstTown, &secondTown, &sizeRoad) != 3) {
+			fclose(file);
 			return fileError;
 		}
 		addRoad(graph, errorCheck, firstTown, secondTown, sizeRoad);
 		if (errorCheck != ok) {
+			clearGraph(graph);
+			fclose(file);
 			return errorCheck;
 		}
 	}
-	printGraph(graph);
+	int numberOfCapitals = 0;
+	if (fscanf_s(file, "%d", &numberOfCapitals) != 1) {
+		clearGraph(graph);
+		fclose(file);
+		return fileError;
+	}
+	int numberCapital;
+	for (int i = 0; i < numberOfCapitals; ++i) {
+		if (fscanf_s(file, "%d", &numberCapital) != 1){
+			clearGraph(graph);
+			fclose(file);
+			return fileError;
+		}
+		addCapital(graph, numberCapital, errorCheck);
+	}
+	fclose(file);
+	toControllTowns(graph, errorCheck);
+	if (errorCheck != ok) {
+		clearGraph(graph);
+		return errorCheck;
+	}
+	int sizeArray = 0;
+	bool** arrayControlledTowns = (bool**)malloc((numberOfTowns + 1) * sizeof(bool*));
+	if (arrayControlledTowns == NULL) {
+		clearGraph(graph);
+		return memoryError;
+	}
+	for (int i = 0; i < numberOfTowns + 1; ++i) {
+		arrayControlledTowns[i] = calloc((numberOfTowns + 1),sizeof(bool*));
+		if (arrayControlledTowns[i] == NULL) {
+			for (int j = 0; j < i; ++j) {
+				free(arrayControlledTowns[j]);
+			}
+			free(arrayControlledTowns);
+			clearGraph(graph);
+			return memoryError;
+		}
+	}
+	returnArrayWithControlledTowns(graph, errorCheck, arrayControlledTowns);
+	if (errorCheck != ok) {
+		for (int i = 0; i < numberOfTowns + 1; ++i) {
+			free(arrayControlledTowns[i]);
+		}
+		free(arrayControlledTowns);
+		clearGraph(graph);
+		return errorCheck;
+	}
+	
+	for (int i = 0; i < numberOfTowns + 1; ++i) {
+		if (arrayControlledTowns[i][0]) {
+			printf("%d: ", i);
+			for (int j = 1; j < numberOfTowns + 1; ++j) {
+				if (arrayControlledTowns[i][j]) {
+					printf("%d ", j - 1);
+				}
+			}
+			printf("\n");
+		}
+	}
+	for (int i = 0; i < numberOfTowns + 1; ++i) {
+		free(arrayControlledTowns[i]);
+	}
+	free(arrayControlledTowns);
+	clearGraph(graph);
 	return ok;
 }
 
 int main() {
+	if (test()) {
+		printf("Tests correct!\n");
+	} else {
+		printf("Error!\n");
+		return -1;
+	}
 	printf("Input fileName, no more than 100 characters\n");
 	char fileName[100] = { '\0' };
-	char checkScanf = scanf("%s", &fileName);
+	char checkScanf = scanf_s("%s", &fileName, 99);
 	while (checkScanf != 1) {
 		while (getchar() != '\n') {
 		}
 		printf("ERROR\n");
-		checkScanf = scanf("%s", &fileName);
+		checkScanf = scanf_s("%s", &fileName, 99);
 	}
-	if (workWithGraph(fileName) != ok) {
-		printf("Error!\n");
+	Error errorCheck = workWithGraph(fileName);
+	switch (errorCheck)
+	{
+	case(ok):
+		return 0;
+	case(memoryError):
+		printf("Error with memory\n");
+		return -1;
+	case(anotherError):
+		printf("Error\n");
+		return -1;
+	case(nullGraph):
+		printf("Error with null graph\n");
+		return -1;
+	case(outOfGraph):
+		printf("Error going beyond the graph\n");
+		return -1;
+	case(fileError):
+		printf("Error with file\n");
 		return -1;
 	}
 }
