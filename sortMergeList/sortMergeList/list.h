e342f1ee235a99e7c0a226de84b41e52ba44d5b7
Artem <Palorel@yandex.ru>
2022-12-23 16:20:04 +0300

    ревью

--- sortMergeList/sortMergeList/list.h
+++ sortMergeList/sortMergeList/list.h
@@ -4,16 +4,23 @@
 
 typedef struct List List;
 
+// Проверка списка на пустоту.
 bool isEmpty(List* list);
 
+// Создание list'а.
 List* createList(void);
 
+// Добавление записи.
 int addRecord(List* list, char name[], char number[]);
 
+// Сортировка.
 void mergeSort(int size, List* list, int numberOrName);
 
+// Проверка на отсортированность.
 bool isSorted(List* list, int numberOrName);
 
+// Очистка list'а
 void clear(List** list);
 
+// Печать list'а
 void printList(List* list);
