dc318d4ec272508eb98d6719e78e9ef5a7e6c551
Artem <Palorel@yandex.ru>
2022-12-23 13:58:25 +0300

    Ревью

--- balancedStaples/balancedStaples/stack.h
+++ balancedStaples/balancedStaples/stack.h
@@ -7,9 +7,16 @@ typedef struct ElementStack {
 	char value;
 } ElementStack;
 
+// Добавление элементов. Возвращает 0 если всё хорошо, иначе возвращает -1 если ошибка.
 int pushElements(ElementStack** head, char value);
+
+// Проверка на пустой стек.
 bool isEmpty(ElementStack* head);
+
+// Очистка стека.
 void clear(ElementStack** head);
+
+// Удаление элементов. Возвращает значение, удалённого элемента
 char popElements(ElementStack** head, int* errorCode);
 
 #endif // !STACK_H_
